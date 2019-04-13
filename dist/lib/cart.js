23d1ed69ae5b72e678c1cf5abe2c6b6e4473d13b
ljw <ljw970812@163.com>
2019-04-13 12:11:38 +0800

    完成商品详情页的JS以及添加购物车的删除功能

--- dist/lib/cart.js
+++ dist/lib/cart.js
@@ -10,7 +10,7 @@
       var products = JSON.parse(data);
       var htmlStr = '';
       var trArray = products.map(function (product) {
-        var tr = "\n                <tr>\n                    <td><label><input type=\"checkbox\"></label></td>\n                    <td><a href=\"javascript:void(0)\">\u4FEE\u6539\u914D\u9001\u65B9\u5F0F</a></td>\n                    <td>\n                        <img src=\"./../resource".concat(product.img, "\" alt=\"\">\n                        <span>").concat(product.description, "</span>\n                    </td>\n                    <td class=\"price\">").concat(product.price, "</td>\n                    <td><button class=\"drop\">-</button><p class=\"count\">0</p><button class=\"add\">+</button></td>\n                    <td class=\"total\">0</td>\n                    <td>\n                        <p><a href=\"javascript:void(0)\">\u79FB\u5165\u6536\u85CF\u5939</a></p>\n                        <p><a href=\"javascript:void(0)\">\u5220\u9664</a></p>\n                        <p><a href=\"javascript:void(0)\">\u67E5\u627E\u76F8\u4F3C</a></p>\n                    </td>\n                </tr>\n                ");
+        var tr = "\n                <tr>\n                    <td><label><input type=\"checkbox\"></label></td>\n                    <td><a href=\"javascript:void(0)\">\u4FEE\u6539\u914D\u9001\u65B9\u5F0F</a></td>\n                    <td>\n                        <img src=\"./../resource".concat(product.img, "\" alt=\"\">\n                        <span>").concat(product.description, "</span>\n                    </td>\n                    <td class=\"price\">").concat(product.price, "</td>\n                    <td><button class=\"drop\">-</button><p class=\"count\">0</p><button class=\"add\">+</button></td>\n                    <td class=\"total\">0</td>\n                    <td>\n                        <p><a href=\"javascript:void(0)\">\u79FB\u5165\u6536\u85CF\u5939</a></p>\n                        <p class=\"deleteFromCart\"><a href=\"javascript:void(0)\">\u5220\u9664</a></p>\n                        <p><a href=\"javascript:void(0)\">\u67E5\u627E\u76F8\u4F3C</a></p>\n                    </td>\n                </tr>\n                ");
         return tr;
       });
       var $tbody = $('tbody');
@@ -47,6 +47,27 @@
       price = coun * price;
       $(this).parent().siblings('.total').html(price);
       sub();
+    });
+    $('.deleteFromCart').on('click', function () {
+      var description = $(this).parent().prevAll().children('span').html();
+      $.ajax({
+        method: 'POST',
+        url: './../UNIQLO/api/deleteFromCart.php',
+        data: {
+          description: "".concat(description)
+        },
+        success: function success(data) {
+          if (JSON.parse(data).result) {
+            alert('删除成功');
+            window.location.href = './cart.html';
+          } else {
+            alert('删除失败');
+          }
+        },
+        error: function error(xhr) {
+          console.error(xhr);
+        }
+      });
     });
   }
 
--- dist/lib/details.js
+++ dist/lib/details.js
@@ -1,40 +1,89 @@
 "use strict";
 
 (function () {
-  $('.view').on('mousemove', function (evt) {
-    console.log('123');
-    var x = evt.offsetX;
-    var y = evt.offsetY;
-    x = x - $('.float').width() / 2;
-    y = y - $('.float').height() / 2;
+  function views() {
+    $('.view').on('mousemove', function (evt) {
+      var x = evt.offsetX;
+      var y = evt.offsetY;
+      x = x - $('.float').width() / 2;
+      y = y - $('.float').height() / 2;
 
-    if (x < 0) {
-      x = 0;
-    }
+      if (x < 0) {
+        x = 0;
+      }
 
-    if (y < 0) {
-      y = 0;
-    }
+      if (y < 0) {
+        y = 0;
+      }
 
-    if (x > $('.view').width() - $('.float').width()) {
-      x = $('.view').width() - $('.float').width();
-    }
+      if (x > $('.view').width() - $('.float').width()) {
+        x = $('.view').width() - $('.float').width();
+      }
 
-    if (y > $('.view').height() - $('.float').height()) {
-      y = $('.view').height() - $('.float').height();
-    }
+      if (y > $('.view').height() - $('.float').height()) {
+        y = $('.view').height() - $('.float').height();
+      }
 
-    $('.float').css({
-      left: x,
-      top: y
+      $('.float').css({
+        // left: x,
+        // top: y
+        borderTopWidth: y,
+        borderRightWidth: 500 - 250 - x,
+        borderBottomWidth: 500 - 250 - y,
+        borderLeftWidth: x
+      });
+      $('.magnify img').css({
+        left: -2 * x,
+        top: -2 * y
+      });
+    }).mouseover(function () {
+      $('.magnify').css({
+        zIndex: 1
+      });
+      $('.float, .magnify img').show();
+      $('.mask').show();
+    }).mouseout(function () {
+      $('.magnify').css({
+        zIndex: -1
+      });
+      $('.float, .magnify img').hide();
+      $('.mask').hide();
     });
-    $('.magnify img').css({
-      left: -2 * x,
-      top: -2 * y
+  }
+
+  views();
+
+  function cart() {
+    $('.cart').on('click', function () {
+      var name = $('.name').html();
+      var price = $('.price').html();
+      var description = $('.tit').html() + " " + name;
+      var imgURL = $($('.magnify img'))[0].src;
+      var imgUrlReg = /\/imgs\/.*/;
+      var img = imgURL.match(imgUrlReg);
+      $.ajax({
+        method: 'POST',
+        url: './../UNIQLO/api/addInCart.php',
+        data: {
+          name: "".concat(name),
+          price: "".concat(price),
+          description: "".concat(description),
+          img: "".concat(img)
+        },
+        success: function success(data) {
+          if (JSON.parse(data).result) {
+            alert('添加成功');
+            window.location.href = './cart.html';
+          } else {
+            alert('添加失败');
+          }
+        },
+        error: function error(xhr) {
+          console.error(xhr);
+        }
+      });
     });
-  }).mouseover(function () {
-    $('.float, .magnify img').show();
-  }).mouseout(function () {
-    $('.float, .magnify img').hide();
-  });
+  }
+
+  cart();
 })();
